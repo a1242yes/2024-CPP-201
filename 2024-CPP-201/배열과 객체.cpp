23a93682d698596ee750471788a549a535a41e49
a1242yes <a01092211242@gmail.com>
2024-03-29 08:54:22 +0900

    arr[i]에서 [i]에는 변경 내용이 들어있다.

---
+++ 2024-CPP-201/배열과 객체.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+	int arr[4] = { 10, 20, 30, 40 };
+	char str[4];
+
+	//두 코드의 결과는 같다
+	printf("%d %d\n", *(&arr[0]), *(&arr[1]));
+	printf("%d %d\n", *(arr), *(arr + 1));
+
+	return 0;
+
+}
